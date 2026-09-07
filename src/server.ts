37a5106a49471d372615d3d43528b7b878143ca4
muhiyadheen <muhiyadheensj@gmail.com>
2026-09-07 16:46:01 +0530

    fix error

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import connectDb from './shared/config/db.js';
+import app from './app.js';
+
+const PORT = process.env.PORT || 5000;
+
+const startServer = async (): Promise<void> => {
+    await connectDb();
+
+    app.listen(PORT, () => {
+        console.log(`SpotNest backend running on port ${PORT}`);
+    });
+};
+
+startServer();
